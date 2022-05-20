0a80099c8b399768a544aa2528e9dd02f2055d61
dod <maulinaro@yahoo.com>
2022-05-20 07:26:10 +0700

    Header & Footer Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,20 @@
-function App() {
-  return <h1>Welcome to proshop</h1>;
-}
+import React from "react";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome To Proshop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
 export default App;
